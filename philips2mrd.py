2a20273941c1922c87221f25b762a486c771734a
MattWill660 <41481302+MattWill660@users.noreply.github.com>
2022-04-08 17:51:55 -0400

    remove ismrmrd subtree and using released package instead

--- philips2mrd.py
+++ philips2mrd.py
@@ -1,8 +1,11 @@
 import os
-from rp.ReadPhilips import PhilipsData
+import ismrmrd as mrd
+from rp.readphilips import *
 
 loc = os.getcwd()
 fname = loc + '\\data\\raw_001.data' #2dim
+
+out = mrd.Acquisition()
 
 inputfile = PhilipsData(fname)
 inputfile.compute()
