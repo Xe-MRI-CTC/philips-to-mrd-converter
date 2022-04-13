57ceca7d655b7c9629953ec3a4b0045689a2aeed
Matthew Willmering <matt.willmering@gmail.com>
2022-04-13 14:45:16 -0400

    Update philips2mrd.py

--- philips2mrd.py
+++ philips2mrd.py
@@ -4,13 +4,8 @@ from rp.ReadPhilips import PhilipsData
 loc = os.getcwd()
 fname = loc + '\\rp\\data\\raw_001.data' #2dim
 
-<<<<<<< Updated upstream
-=======
-#fname = 'fakeloc_asdaskdfjak'
-os.path.exists(fname)
 out = mrd.Acquisition()
 
->>>>>>> Stashed changes
 inputfile = PhilipsData(fname)
 inputfile.compute()
 
